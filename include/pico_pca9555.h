d5ab2ec2ee026aa9fa7bcd5a9701c7b261f4b93f
Edward Hesketh <inbox@edwardh.dev>
2024-11-16 19:41:42 +0000

    feat!: faster input reading, change read returns
    BREAKING CHANGE: read-based functions return their value, instead of
    writing it to a value at the pointed address.

--- include/pico_pca9555.h
+++ include/pico_pca9555.h
@@ -5,57 +5,54 @@
 
 // Read the input values of the pca9555. The value is written to the object at
 // the inputs pointer.
-int pca9555_read_input(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                       uint16_t *inputs);
+uint16_t pca9555_read_input(i2c_inst_t *i2c_instance, uint8_t i2c_address);
 
 // Read the input values of the first 8 pins (port 0) of the pca9555. The value
 // is written to the object at the inputs pointer.
-int pca9555_read_input_0(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                         uint8_t *inputs);
+uint8_t pca9555_read_input_0(i2c_inst_t *i2c_instance, uint8_t i2c_address);
 
 // Read the input values of the last 8 pins (port 1) of the pca9555. The value
 // is written to the object at the inputs pointer.
-int pca9555_read_input_1(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                         uint8_t *inputs);
+uint8_t pca9555_read_input_1(i2c_inst_t *i2c_instance, uint8_t i2c_address);
 
 // Configure the inversion of the input read of the pca9555. A 0 bit is not
 // inverted, 1 is inverted.
-int pca9555_input_inversion(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                            uint16_t inverted_pins);
+void pca9555_input_inversion(i2c_inst_t *i2c_instance, uint8_t i2c_address,
+                             uint16_t inverted_pins);
 
 // Configure the inversion of the input read of the first 8 pins (port 0). A 0
 // bit is not inverted, 1 is inverted.
-int pca9555_input_inversion_0(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                              uint8_t inverted_pins);
+void pca9555_input_inversion_0(i2c_inst_t *i2c_instance, uint8_t i2c_address,
+                               uint8_t inverted_pins);
 
 // Configure the inversion of the input read of the last 8 pins (port 1). A 0
 // bit is not inverted, 1 is inverted.
-int pca9555_input_inversion_1(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                              uint8_t inverted_pins);
+void pca9555_input_inversion_1(i2c_inst_t *i2c_instance, uint8_t i2c_address,
+                               uint8_t inverted_pins);
 
 // Configure the pins for the pca9555. A 0 bit is output, 1 is input
-int pca9555_configure(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                      uint16_t configuration);
+void pca9555_configure(i2c_inst_t *i2c_instance, uint8_t i2c_address,
+                       uint16_t configuration);
 
 // Configure the first 8 pins of the pca9555 (port 0). A 0 bit is output, 1 is
 // input
-int pca9555_configure_0(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                        uint8_t configuration);
+void pca9555_configure_0(i2c_inst_t *i2c_instance, uint8_t i2c_address,
+                         uint8_t configuration);
 
 // Configure the last 8 pins of the pca9555 (port 1). A 0 bit is output, 1 is
 // input
-int pca9555_configure_1(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                        uint8_t configuration);
+void pca9555_configure_1(i2c_inst_t *i2c_instance, uint8_t i2c_address,
+                         uint8_t configuration);
 
 // Set the output values of the pca9555
-int pca9555_output(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                   uint16_t output_value);
+void pca9555_output(i2c_inst_t *i2c_instance, uint8_t i2c_address,
+                    uint16_t output_value);
 
 // Set the output values of the first 8 pins (port 0)
-int pca9555_output_0(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                     uint8_t output_value);
+void pca9555_output_0(i2c_inst_t *i2c_instance, uint8_t i2c_address,
+                      uint8_t output_value);
 
 // Set the output values of the last 8 pins (port 1)
-int pca9555_output_1(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                     uint8_t output_value);
+void pca9555_output_1(i2c_inst_t *i2c_instance, uint8_t i2c_address,
+                      uint8_t output_value);
 #endif
--- src/pico_pca9555.c
+++ src/pico_pca9555.c
@@ -1,135 +1,115 @@
 #include "pico_pca9555.h"
 
 // Constants for I2C communication
-const uint8_t PCA9555_CMD_READ_INPUTS_0 = _u(0x00);
-const uint8_t PCA9555_CMD_READ_INPUTS_1 = _u(0x01);
-const uint8_t PCA9555_CMD_SET_OUTPUTS_0 = _u(0x02);
-const uint8_t PCA9555_CMD_SET_OUTPUTS_1 = _u(0x03);
-const uint8_t PCA9555_CMD_POLARITY_INVERT_0 = _u(0x04);
-const uint8_t PCA9555_CMD_POLARITY_INVERT_1 = _u(0x05);
-const uint8_t PCA9555_CMD_CONFIGURE_0 = _u(0x06);
-const uint8_t PCA9555_CMD_CONFIGURE_1 = _u(0x07);
+const uint8_t PCA9555_CMD_READ_INPUTS_0 = 0x00;
+const uint8_t PCA9555_CMD_READ_INPUTS_1 = 0x01;
+const uint8_t PCA9555_CMD_SET_OUTPUTS_0 = 0x02;
+const uint8_t PCA9555_CMD_SET_OUTPUTS_1 = 0x03;
+const uint8_t PCA9555_CMD_POLARITY_INVERT_0 = 0x04;
+const uint8_t PCA9555_CMD_POLARITY_INVERT_1 = 0x05;
+const uint8_t PCA9555_CMD_CONFIGURE_0 = 0x06;
+const uint8_t PCA9555_CMD_CONFIGURE_1 = 0x07;
 
-int pca9555_read_input(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                       uint16_t *inputs) {
-  // Two sets of 8-bit inputs
-  uint8_t inputs_0;
-  uint8_t inputs_1;
+uint16_t pca9555_read_input(i2c_inst_t *i2c_instance, uint8_t i2c_address) {
   // Read the 8-bit input values
-  int err = pca9555_read_input_0(i2c_instance, i2c_address, &inputs_0);
-  if (err < 0) {
-    return err;
-  }
-  err = pca9555_read_input_1(i2c_instance, i2c_address, &inputs_1);
-  if (err < 0) {
-    return err;
-  }
-  // Combine the 8-bit inputs into a 16-bit value
-  *inputs = (inputs_1 << 8) | inputs_0;
-  return 0;
+  uint8_t inputs_read_cmd[1] = {PCA9555_CMD_READ_INPUTS_0};
+  i2c_write_blocking(i2c_instance, i2c_address, inputs_read_cmd, 1, false);
+  uint8_t inputs[2];
+  i2c_read_blocking(i2c_instance, i2c_address, inputs, 2, false);
+  return (inputs[1] << 8) | inputs[0];
 }
 
-int pca9555_read_input_0(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                         uint8_t *inputs) {
+uint8_t pca9555_read_input_0(i2c_inst_t *i2c_instance, uint8_t i2c_address) {
   // Read the 8-bit input value
-  i2c_write_blocking(i2c_instance, i2c_address, &PCA9555_CMD_READ_INPUTS_0, 1,
-                     true);
-  return i2c_read_blocking(i2c_instance, i2c_address, inputs, 1, false);
+  uint8_t inputs_read_cmd_0[1] = {PCA9555_CMD_READ_INPUTS_0};
+  i2c_write_blocking(i2c_instance, i2c_address, inputs_read_cmd_0, 1, false);
+  uint8_t inputs[1];
+  i2c_read_blocking(i2c_instance, i2c_address, inputs, 1, false);
+  return inputs[0];
 }
 
-int pca9555_read_input_1(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                         uint8_t *inputs) {
+uint8_t pca9555_read_input_1(i2c_inst_t *i2c_instance, uint8_t i2c_address) {
   // Read the 8-bit input value
-  i2c_write_blocking(i2c_instance, i2c_address, &PCA9555_CMD_READ_INPUTS_1, 1,
-                     true);
-  return i2c_read_blocking(i2c_instance, i2c_address, inputs, 1, false);
+  uint8_t inputs_read_cmd_1[1] = {PCA9555_CMD_READ_INPUTS_1};
+  i2c_write_blocking(i2c_instance, i2c_address, inputs_read_cmd_1, 1, false);
+  uint8_t inputs[1];
+  i2c_read_blocking(i2c_instance, i2c_address, inputs, 1, false);
+  return inputs[0];
 }
 
-int pca9555_configure_0(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                        uint8_t configuration) {
+void pca9555_configure_0(i2c_inst_t *i2c_instance, uint8_t i2c_address,
+                         uint8_t configuration) {
   // Set the 8-bit configuration
   uint8_t config_0[2] = {PCA9555_CMD_CONFIGURE_0, configuration};
   // Write the configuration
-  return i2c_write_blocking(i2c_instance, i2c_address, config_0, 2, false);
+  i2c_write_blocking(i2c_instance, i2c_address, config_0, 2, false);
 }
 
-int pca9555_configure_1(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                        uint8_t configuration) {
+void pca9555_configure_1(i2c_inst_t *i2c_instance, uint8_t i2c_address,
+                         uint8_t configuration) {
   // Set the 8-bit configuration
   uint8_t config_1[2] = {PCA9555_CMD_CONFIGURE_1, configuration};
   // Write the configuration
-  return i2c_write_blocking(i2c_instance, i2c_address, config_1, 2, false);
+  i2c_write_blocking(i2c_instance, i2c_address, config_1, 2, false);
 }
 
-int pca9555_input_inversion(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                            uint16_t inverted_pins) {
+void pca9555_input_inversion(i2c_inst_t *i2c_instance, uint8_t i2c_address,
+                             uint16_t inverted_pins) {
   // Two sets of 8-bit input inversion
   uint8_t inversion_0[2] = {PCA9555_CMD_POLARITY_INVERT_0,
                             inverted_pins & 0xFF};
   uint8_t inversion_1[2] = {PCA9555_CMD_POLARITY_INVERT_1,
                             (inverted_pins >> 8) & 0xFF};
-  int err =
-      pca9555_input_inversion_0(i2c_instance, i2c_address, inversion_0[1]);
-  if (err < 0) {
-    return err;
-  }
-  return pca9555_input_inversion_1(i2c_instance, i2c_address, inversion_1[1]);
+  pca9555_input_inversion_0(i2c_instance, i2c_address, inversion_0[1]);
+  pca9555_input_inversion_1(i2c_instance, i2c_address, inversion_1[1]);
 }
 
-int pca9555_input_inversion_0(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                              uint8_t inverted_pins) {
+void pca9555_input_inversion_0(i2c_inst_t *i2c_instance, uint8_t i2c_address,
+                               uint8_t inverted_pins) {
   // Set the 8-bit input inversion
   uint8_t inversion_0[2] = {PCA9555_CMD_POLARITY_INVERT_0, inverted_pins};
   // Write the inversion
-  return i2c_write_blocking(i2c_instance, i2c_address, inversion_0, 2, false);
+  i2c_write_blocking(i2c_instance, i2c_address, inversion_0, 2, false);
 }
 
-int pca9555_input_inversion_1(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                              uint8_t inverted_pins) {
+void pca9555_input_inversion_1(i2c_inst_t *i2c_instance, uint8_t i2c_address,
+                               uint8_t inverted_pins) {
   // Set the 8-bit input inversion
   uint8_t inversion_1[2] = {PCA9555_CMD_POLARITY_INVERT_1, inverted_pins};
   // Write the inversion
-  return i2c_write_blocking(i2c_instance, i2c_address, inversion_1, 2, false);
+  i2c_write_blocking(i2c_instance, i2c_address, inversion_1, 2, false);
 }
 
-int pca9555_output(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                   uint16_t output_value) {
+void pca9555_output(i2c_inst_t *i2c_instance, uint8_t i2c_address,
+                    uint16_t output_value) {
   // Two sets of 8-bit outputs
-  int err = pca9555_output_0(i2c_instance, i2c_address, output_value & 0xFF);
-  if (err < 0) {
-    return err;
-  }
-  return pca9555_output_1(i2c_instance, i2c_address,
-                          (output_value >> 8) & 0xFF);
+  pca9555_output_0(i2c_instance, i2c_address, output_value & 0xFF);
+  pca9555_output_1(i2c_instance, i2c_address, (output_value >> 8) & 0xFF);
 }
 
-int pca9555_output_0(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                     uint8_t output_value) {
+void pca9555_output_0(i2c_inst_t *i2c_instance, uint8_t i2c_address,
+                      uint8_t output_value) {
   // Set the 8-bit outputs
   uint8_t outputs_0[2] = {PCA9555_CMD_SET_OUTPUTS_0, output_value};
   // Write the outputs
-  return i2c_write_blocking(i2c_instance, i2c_address, outputs_0, 2, false);
+  i2c_write_blocking(i2c_instance, i2c_address, outputs_0, 2, false);
 }
 
-int pca9555_output_1(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                     uint8_t output_value) {
+void pca9555_output_1(i2c_inst_t *i2c_instance, uint8_t i2c_address,
+                      uint8_t output_value) {
   // Set the 8-bit outputs
   uint8_t outputs_1[2] = {PCA9555_CMD_SET_OUTPUTS_1, output_value};
   // Write the outputs
-  return i2c_write_blocking(i2c_instance, i2c_address, outputs_1, 2, false);
+  i2c_write_blocking(i2c_instance, i2c_address, outputs_1, 2, false);
 }
 
 // Configure the PCA9555 for inputs or outputs
-int pca9555_configure(i2c_inst_t *i2c_instance, uint8_t i2c_address,
-                      uint16_t configuration) {
+void pca9555_configure(i2c_inst_t *i2c_instance, uint8_t i2c_address,
+                       uint16_t configuration) {
   // Two sets of 8-bit configurations
   uint8_t config_0[2] = {PCA9555_CMD_CONFIGURE_0, configuration & 0xFF};
   uint8_t config_1[2] = {PCA9555_CMD_CONFIGURE_1, (configuration >> 8) & 0xFF};
   // Write the configurations
-  int err = i2c_write_blocking(i2c_instance, i2c_address, config_0, 2, true);
-  if (err < 0) {
-    return err;
-  }
-  err = i2c_write_blocking(i2c_instance, i2c_address, config_1, 2, false);
-  return err;
+  i2c_write_blocking(i2c_instance, i2c_address, config_0, 2, true);
+  i2c_write_blocking(i2c_instance, i2c_address, config_1, 2, false);
 }
